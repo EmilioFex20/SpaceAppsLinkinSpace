b5321a86180d518f2c5128ad6f7d4cbbcb5d4446
EmilioFex20 <emilio.pouget@cetys.edu.mx>
2024-10-06 23:41:37 -0700

    CAMBIO FINAL

---
+++ orrery-app/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: 'SpaceAppsLinkinSpace', // Reemplaza 'nombre-del-repo' por el nombre de tu repositorio de GitHub
+  build: {
+    outDir: 'docs', // Cambia la carpeta de salida a 'docs' para GitHub Pages
+  }
+});
