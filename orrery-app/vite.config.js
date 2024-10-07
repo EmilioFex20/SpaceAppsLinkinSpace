48c5a8a1d00b0d955824211f3ee599e54393bfd0
EmilioFex20 <emilio.pouget@cetys.edu.mx>
2024-10-07 00:53:58 -0700

    Update vite.config.js

--- orrery-app/vite.config.js
+++ orrery-app/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import vanilla from '@vite/js/plugins-vanilla'
 
 export default defineConfig({
   base: '/SpaceAppsLinkinSpace/', // Reemplaza 'nombre-del-repo' por el nombre de tu repositorio de GitHub
+  plugins: [vanilla()]
 });
